78c8e4fc73369427c0a4f55b56bd7b376de3105a
MarkusMaal2 <markus.maal@voco.ee>
2024-01-23 12:51:30 +0200

    Global dark purple styling

--- src/components/Private/Header.js
+++ src/components/Private/Header.js
@@ -3,7 +3,7 @@ import FormButton from "../LoginForm/FormButton";
 
 const Header = (props) => {
     return (
-        <nav className="navbar navbar-light bg-light p-3">
+        <nav className="navbar navbar-dark p-3">
             <a className="navbar-brand col-lg-12 text-center" href="#">{props.label}</a>
         </nav>
     )
--- src/components/Private/Note.js
+++ src/components/Private/Note.js
@@ -7,8 +7,8 @@ const Note = (props) => {
     }
     //console.log(props.note.id);
     return (
-        <div className={"rounded-3 mb-3 border border-primary-subtle mx-0"}>
-            <div className={"text-bg-primary rounded-top-3 text-center note-header"}><p className={"p-2"}>Muudetud: {props.note.MODIFIED}</p>
+        <div className={"rounded-3 mb-3 border border-primary-subtle mx-0"} style={"border-color: #8464a2 !important;"}>
+            <div className={"text-bg-primary rounded-top-3 text-center note-header"} style={"background-color: #8464a2 !important;"}><p className={"p-2"}>Muudetud: {props.note.MODIFIED}</p>
             </div>
             <p className={"px-3"}>{nl2br(props.note.CONTENT)}</p>
             <div className={"rounded-bottom-3 text-bg-light position-relative pb-5"} note_id={props.note.id}>
