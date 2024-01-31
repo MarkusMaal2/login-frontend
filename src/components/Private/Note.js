26f1dfd1eb4518241a36ff0a2210261821bf4498
MarkusMaal2 <markus.maal@voco.ee>
2024-01-31 16:06:12 +0200

    Change MODIFIED field name to updatedAt

--- src/components/Private/Note.js
+++ src/components/Private/Note.js
@@ -8,7 +8,7 @@ const Note = (props) => {
     //console.log(props.note.id);
     return (
         <div className={"rounded-3 mb-3 border border-secondary mx-0"}>
-            <div className={"rounded-top-3 text-center note-header"}><p className={"p-2"}>Muudetud: {props.note.MODIFIED}</p>
+            <div className={"rounded-top-3 text-center note-header"}><p className={"p-2"}>Muudetud: {props.note.updatedAt}</p>
             </div>
             <p className={"px-3"}>{nl2br(props.note.CONTENT)}</p>
             <div className={"rounded-bottom-3 background-dark position-relative pb-5"} note_id={props.note.id}>
